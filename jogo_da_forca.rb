6f8f038c29d0900b0631c4068b64f599824dcc4c
Renan Tavares <renan_velox@hotmail.com>
2017-09-22 17:59:53 -0300

    criando a funçao querJogar?

--- jogo_da_forca.rb
+++ jogo_da_forca.rb
@@ -2,6 +2,7 @@ def boasVidas
     puts "Bem vindo ao jogo da forca"
     puts "Qual é seu nome? "
     nome = gets.strip
+    puts "\n\n\n\n\n"
     puts " começaremos o jogo para você " + nome.to_s
     nome
 end
@@ -9,6 +10,12 @@ end
 def escolhePalavraSecreta
     puts "Escolhendo uma palavra secreta para você"
     palavraSecreta.upcase = "programador"
-    puts "A palavra secreta com " + palavraSecreta.size.to_s + "letra.. Boa Sorte!"
+    puts "A palavra secreta com " + palavraSecreta.size.to_s + " letra.. Boa Sorte!"
     palavraSecreta
+end
+
+def querJogar?
+    puts "Deseja Jogar Novamente? (S/N)"
+    queroJogar = gets.strip
+    querJogar = queroJogar.upcase == "S"
 end
